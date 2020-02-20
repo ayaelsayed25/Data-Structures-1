3ee869316c361953f622df8d043846eac1bf18bf
Aya Elsayed <45416886+ayaelsayed25@users.noreply.github.com>
2020-02-20 14:55:34 +0200

    Add files via upload

---
+++ SimpleCalc.java
@@ -0,0 +1,16 @@
+public class SimpleCalc implements ICalculator{
+
+    public int add(int x, int y)
+    {
+        return x + y ;
+    }
+    public float divide(int x, int y)
+    {
+        if(y == 0)
+        {
+            throw new RuntimeException("Can't divide by zero!");
+        }
+        return x/y;
+    }
+    
+}
